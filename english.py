a894e1cc4f4fe3a94fea98dbf062b341ef978059
Luke Patton <156141857+lukerpatton@users.noreply.github.com>
2024-05-12 14:24:30 -0500

    Add files via upload

--- english.py
+++ english.py
@@ -2,11 +2,19 @@ import random
 
 def main():
     facts = [
-        "Python was created by Guido van Rossum and first released in 1991.",
-        "Python is named after the British comedy series 'Monty Python’s Flying Circus'.",
+        "Python is taught at Seven Lakes High School in Computer Science class",
+        "The current version of Python is 3",
+        "NASA uses Python for some of its programming tasks, including running a space probe!",
+        "Python is great for automating repetitive tasks that save time and reduce the chance of errors.",
+        "You don't need to declare the type of a variable in Python, unlike many other programming languages.",
+        "One of the world's most popular social media platforms, Instagram, uses Python on its backend.",
+        "Python programs can run on any operating system, such as Windows, macOS, Linux, and even Raspberry Pi.",
+        "Python is open-source, which means it's free to use and distribute, even for commercial purposes.",
+        "Python was created by Guido van Rossum over the Christmas holiday in 1989",
+        "Python was first released in 1991",
+        "Python is not named after the snake, it is named after the British comedy series 'Monty Python's Flying Circus'.",
         "Python is used by many large companies, including Google, Netflix, and Spotify.",
-        "Python's design philosophy emphasizes code readability with its notable use of significant whitespace.",
-        "Python is an interpreted language, which means that the written code is not actually translated to a computer-readable format at runtime."
+        "Python's design philosophy emphasizes code readability."
     ]
     
     while True:
